45fac1f9ac8b9b48bf55d0a6b9ef0ecc5ecf159b
dju4mota <fernando.b@ges.inatel.com>
2020-05-02 22:41:19 -0300

    Comentários no PerceptronSimples

--- PerceptronSimples.py
+++ PerceptronSimples.py
@@ -4,7 +4,6 @@ entradas = [[-1, 0.1, 0.4, 0.7],
             [-1, 0.5, 0.7, 0.1]]
 
 respostas = [1, -1, -1, 1]
-# saidas = []
 epocas = 0
 taxa = 0.085
 erro = False
@@ -16,15 +15,15 @@ def novoPeso(y, u, x):   # y resposta esperada, u resposta calculada, x entrada
         pesos[i] = pesos[i] + (taxa * (y - u) * x[i])
 
 
-def multiplica(entrada):
+def multiplica(ent):
     u = []
     for x in range(0, 4):
-        u.append(entrada[x] * pesos[x])
+        u.append(ent[x] * pesos[x])
     return u
 
 
-def step(u):
-    # return round(u, 0)
+def step(u):   # degrau bipolar
+    # return round(u, 0) # função degrau
     if u > 0:
         return 1
     elif u < 0:
@@ -33,29 +32,32 @@ def step(u):
 
 
 def soma(lista):
-    soma = 0
+    s = 0           # soma total
     valores = multiplica(lista)
     for x in valores:
-        soma += x
-    return step(soma)
+        s += x
+    return step(s)
 
 
+# treinamento
 while True:  # emulate do while
-    erro = False
-    for f in range(0, 4):
-        saida = (soma(entradas[f]))
+    erro = False     # so para com a resposta 100% certa, só é possível em problemas lineares
+    for f in range(0, 4):   # numero de casos de testes e nao de entradas
+        saida = (soma(entradas[f]))  # uma variavel única pois da trabalho resetar a lista a cada treinamento
         if saida != respostas[f]:
             erro = True
             novoPeso(respostas[f], saida, entradas[f])
             print(f"Novos pesos: {pesos}")
-    epocas += 1
+    epocas += 1      # numero de treinamentos
     if not erro:    # emulate do while
         break
 
 
+# depois da rede estar treinada - pronta
 saidas = []
 for entrada in entradas:
-    saidas.append(soma(entrada))
+    saidas.append(soma(entrada))   # calcula as entradas uma última vez com os pesos corretos
+
 
 print(f"Saida: {saidas}")
 print(f"Pesos Iniciais: {[0.2, 0.3, 0.4, 0.5]}")
